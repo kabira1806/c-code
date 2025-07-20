31c23c19bf07857151353b3c9be2f06ccb733d13
Divyam1806 <djaingulgulia@gmail.com>
2025-07-20 22:07:03 +0530

    dsa

--- arraypointers.cpp
+++ arraypointers.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
 
-    int arr[]={1,2,3,4}; //arr is a pointer
+    int arr[]={1,2,3,5}; //arr is a pointer
     cout << arr<< endl;
     cout << *arr;//value of 0th index will be printed
     return 0;
